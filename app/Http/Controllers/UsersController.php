abd22371a71b7ef83fea1fa035782d5830300d8d
ming <124429474@qq.com>
2019-09-21 15:48:52 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,6 +27,13 @@ class UsersController extends Controller
     }
 
 
+    public function index()
+    {
+//        $users = User::all();
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
     public function show(User $user)
     {
         return view('users.show',compact('user'));
