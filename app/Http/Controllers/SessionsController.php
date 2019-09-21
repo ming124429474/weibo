72424d655df387efe7632c202cc6864d4690f3ac
ming <124429474@qq.com>
2019-09-21 15:08:46 +0800

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,14 @@ use Auth;
 class SessionsController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only'=>['create'],
+        ]);
+    }
+
+
     public function create()
     {
         return view('sessions.create');
@@ -23,7 +31,10 @@ class SessionsController extends Controller
         if(Auth::attempt($credentials,$request->has('remember'))){
             // 登录成功后的相关操作
             session()->flash('success','欢迎回来!');
-            return redirect()->route('users.show',[Auth::user()]);
+//            return redirect()->route('users.show',[Auth::user()]);
+            //用户页面重定向
+            $fallback = route('users.show',Auth::user());
+            return redirect()->intended($fallback);
         }else{
             // 登录失败后的相关操作
             session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
