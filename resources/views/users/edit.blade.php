b97b2956f7b155020a58fb1a0ec5fd099ffa58bf
ming <124429474@qq.com>
2019-09-21 11:35:38 +0800

    更改用户资料

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -39,7 +39,7 @@
 
                     <div class="form-group">
                         <label for="password_confirmation">确认密码：</label>
-                        <input type="password" name="password" class="form-control" value="{{ old('password_confirmation') }}">
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
                     </div>
 
                     <button type="submit" class="btn btn-primary">更新</button>
