9c669fb8ca46c33299367ea8fbf31c8ae4904deb
ming <124429474@qq.com>
2019-09-24 14:21:40 +0800

    用户激活

---
+++ resources/views/email/confirm.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>注册确认链接</title>
+</head>
+<body>
+<h1>感谢您在 Weibo App 网站进行注册！</h1>
+<p>
+    请点击下面的链接完成注册：
+    <a href="{{route('confirm_email',$user->activation_token)}}">
+        {{route('confirm_email',$user->activation_token)}}
+    </a>
+</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,6 @@ Route::resource('users','UsersController');
 Route::get('login','SessionsController@create')->name('login');
 Route::post('login','SessionsController@store')->name('login');
 Route::delete('logout','sessionsController@destroy')->name('logout');
+
+//邮箱验证
+Route::get('signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
